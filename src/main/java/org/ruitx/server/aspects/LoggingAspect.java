4492b1cc6b5dc3a1b1cf39e82f77ef74258061c9
ruitx <rui.teixeira@minderacodeacademy.com>
2024-10-31 15:00:50 +0000

    Removed old code.

--- src/main/java/org/ruitx/server/aspects/LoggingAspect.java
+++ src/main/java/org/ruitx/server/aspects/LoggingAspect.java
@@ -4,7 +4,6 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.ruitx.server.components.Yggdrasill;
-import org.ruitx.server.strings.Messages;
 import org.tinylog.Logger;
 
 @Aspect
@@ -12,8 +11,7 @@ public class LoggingAspect {
 
     @Before("execution(* org.ruitx.server.components.Yggdrasill.start(..))")
     public void serverStart(JoinPoint joinPoint) {
-        Logger.info("{} at port {} with www path: {}",
-                Messages.SERVER_STARTED,
+        Logger.info("JAWS started at port {} with www path: {}",
                 Yggdrasill.currentPort,
                 Yggdrasill.currentResourcesPath);
     }
